5488a1937f73e3bd8893b7277f59293ebc4e0511
Malindu <gimsaramalindu2002@gmail.com>
2025-06-28 18:11:15 +0530

    login page and admin page

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 export default function AdminPage() {
     return (
-        <div className='w-full bg-blue-500 h-screen flex'>
-            <div className=''>
-                Admin Dashboard
+        <div className='w-full h-screen flex'>
+            <div className='h-full w-[300px] bg-red-300'>
+                <Link to ="/admin/users" className="block p-2">Users</Link>
+                <Link to ="/admin/product" className="block p-2">Product</Link>
+                <Link to ="/admin/orders" className="block p-2">Orders</Link>
+            </div>
+
+            <div className="h-full bg-white w-[calc(100vw-300px)] flex rounded-lg">
+                <Routes path="/*">
+                    <Route path="/users" element={<div>User</div>} />
+                    <Route path="/product" element={<div>Product</div>} />
+                    <Route path="/orders" element={<div>Orders</div>} />
+                </Routes>
             </div>
         </div>
     )
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,18 @@
 function LoginPage(){
     return(
         <div className='w-full bg-red-500 h-screen bg-[url(/loginbg.jpg)] bg-cover bg-center flex'>
-            <h1>Login</h1>
+            <div className='w-[50%] h-full'>
+            </div>
+
+            <div className='w-[50%] h-full flex justify-center items-center '>
+                <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center">
+                    <input className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="text" placeholder="email" />
+                    <input className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="password" placeholder="password" />
+                    <button className="w-[400px] h[50px] bg-green-500 text-white rounded-xl text-center m-[5px] cursor-pointer">Login</button>
+                </div>
+            </div>
+
+            
         </div>
     )
 } 
