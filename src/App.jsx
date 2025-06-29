19fc9893ff6761707c73ea32797742ee6dac427d
Malindu <gimsaramalindu2002@gmail.com>
2025-06-29 10:47:27 +0530

    day 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LoginPage from './pages/loginPage'
 import AdminPage from './pages/adminPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   
   return (
     <BrowserRouter>
+    <Toaster position="top-center" /> 
       <Routes path="/*">
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/login" element={<LoginPage />} />
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 function LoginPage(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    function handleLogin() {
+        console.log("Email:", email);
+        console.log("Password:", password);
+        
+         axios.post(import.meta.env.VITE_BACKEND_URL+"/api/user/login", {
+            email: email,
+            password: password
+        }).then((response) => {
+            toast.success("Login successful!");
+            localStorage.setItem("token", response.data.token);
+
+            const user = response.data.user;
+            if(user.role =="admin"){
+                window.location.href = "/admin";
+            }
+            else {
+                 window.location.href = "/"
+            }
+               
+        }).catch((error) => {
+            console.error("Login failed:", error);
+            toast.error(error.response.data.message || "Login failed. Please try again.");
+           
+        } )
+
+    }
+
     return(
         <div className='w-full bg-red-500 h-screen bg-[url(/loginbg.jpg)] bg-cover bg-center flex'>
             <div className='w-[50%] h-full'>
             </div>
 
             <div className='w-[50%] h-full flex justify-center items-center '>
-                <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center">
-                    <input className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="text" placeholder="email" />
-                    <input className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="password" placeholder="password" />
-                    <button className="w-[400px] h[50px] bg-green-500 text-white rounded-xl text-center m-[5px] cursor-pointer">Login</button>
+                <div className="w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-lg flex flex-col justify-center items-center">
+                    <input onChange={(e) => setEmail(e.target.value)}
+                    className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="text" placeholder="email" />
+                    <input onChange={(e) => setPassword(e.target.value)}
+                    className="w-[400px] h[50px] border border-white rounded-xl text-center m-[5px]" type="password" placeholder="password" />
+                    <button onClick={handleLogin} className="w-[400px] h[50px] bg-green-500 text-white rounded-xl text-center m-[5px] cursor-pointer">Login</button>
                 </div>
             </div>
 
